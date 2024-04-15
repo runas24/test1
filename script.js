118da7ff9b286791b58acf140a1a5c7ae85e8328
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-15 15:35:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     var submitButton = document.querySelector('button[type="submit"]');
     submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
-    // Показываем прогресс
+    // Показываем прогресс и текст "Заявка на рассмотрении"
     document.getElementById("progressBar").style.display = "block";
     document.querySelector(".progress").style.width = "0%";
+    document.querySelector(".progress-text").style.display = "block";
 
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
@@ -25,9 +26,9 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     setTimeout(function() {
         submitButton.disabled = false; // Включаем кнопку после загрузки
 
+        // Показываем результат и текст "Ваша заявка предварительно одобрена"
         document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
-
-        // Скрываем прогресс
         document.getElementById("progressBar").style.display = "none";
+        document.getElementById("result").style.display = "block";
     }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
 });
