9d40c2131ad5ada41e8cade8c98b021a237df39d
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-15 14:35:57 +0500

    script.js

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ function formatCurrency(input) {
 document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var submitButton = document.getElementById('submitButton');
+    var submitButton = document.querySelector('button[type="submit"]');
     submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
     // Добавляем сообщение о загрузке
-    var loadingMessage = document.createElement("div");
+    var loadingMessage = document.querySelector('.result-container .loading-message');
     loadingMessage.innerText = "Ваша заявка на рассмотрении...";
-    loadingMessage.classList.add("loading-message");
-    document.getElementById("maxLoanAmount").appendChild(loadingMessage);
 
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
@@ -25,17 +23,11 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
 
     setTimeout(function() {
-        // Удаляем сообщение о загрузке
-        loadingMessage.remove();
-
         submitButton.disabled = false; // Включаем кнопку после загрузки
 
-        var maxLoanAmountElement = document.getElementById("maxLoanAmount");
-        maxLoanAmountElement.innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
-        maxLoanAmountElement.style.opacity = 1; // Плавное появление результата
+        // Удаляем сообщение о загрузке
+        loadingMessage.innerText = "";
+
+        document.querySelector('.result-container #maxLoanAmount').innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
     }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
-
-    // Добавляем анимацию к кнопке "Рассчитать"
-    submitButton.classList.add("loading");
 });
-
