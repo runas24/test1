eaa3378f7775de6c06394076c364c1f8e002e66e
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-15 17:09:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,24 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
 
     // Simulate processing delay
     setTimeout(function() {
-        overlay.style.display = "none";
-        progressText.innerText = "Ваша заявка предварительно одобрена";
-    }, 10000);
-});
+        var submitButton = document.querySelector('button[type="submit"]');
+        submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
+        var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
+        var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
+        var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
+
+        // Рассчитываем максимальную сумму кредита
+        // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
+        var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
+
+        setTimeout(function() {
+            submitButton.disabled = false; // Включаем кнопку после загрузки
+
+            overlay.style.display = "none";
+            progressText.innerText = "Ваша заявка предварительно одобрена";
+
+            document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
+        }, 1000); // Здесь можно установить другое значение задержки, если нужно
+    }, 5000); // Здесь можно установить другое значение задержки, если нужно
+});
