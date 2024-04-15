abd03c6a64fbc54a87044b95c695b9aa08c38ad0
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-15 17:09:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,33 +7,15 @@ function formatCurrency(input) {
 document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var submitButton = document.querySelector('button[type="submit"]');
-    submitButton.disabled = true; // Отключаем кнопку во время загрузки
+    var overlay = document.getElementById("overlay");
+    var progressText = document.querySelector('.progress-text');
 
-    var overlay = document.getElementById('overlay');
-    overlay.style.display = 'flex'; // Показываем оверлей
+    overlay.style.display = "flex";
 
-    var progressBar = document.getElementById('progressBar');
-    var overlayText = document.getElementById('overlayText');
-    progressBar.style.width = '0%'; // Устанавливаем начальную ширину прогресс бара
+    // Simulate processing delay
+    setTimeout(function() {
+        overlay.style.display = "none";
+        progressText.innerText = "Ваша заявка предварительно одобрена";
+    }, 10000);
+});
 
-    var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
-    var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
-    var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
-
-    var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
-
-    var progress = 0;
-    var interval = setInterval(function() {
-        progressBar.style.width = progress + '%'; // Обновляем ширину прогресс бара
-        progress++;
-        if (progress >= 100) {
-            clearInterval(interval); // Останавливаем интервал после достижения 100%
-            setTimeout(function() {
-                submitButton.disabled = false; // Включаем кнопку после загрузки
-                overlay.style.display = 'none'; // Скрываем оверлей
-                document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
-                overlayText.innerText = "Ваша заявка предварительно одобрена"; // Обновляем текст оверлея
-            }, 1000); // Дополнительная задержка перед отображением результата
-        }
-    }, 100); // Интервал обновления
