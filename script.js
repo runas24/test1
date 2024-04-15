49b7aff81e72d48a9c28a5cb54023424d176fb16
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-15 15:56:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,31 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
     var overlay = document.getElementById("overlay");
-    var loadingText = document.getElementById("loadingText");
-    var approvalText = document.getElementById("approvalText");
     overlay.style.display = "flex";
-    loadingText.style.display = "block";
+
+    var loadingMessage = document.getElementById("loadingMessage");
+    loadingMessage.style.display = "block";
+
+    var resultMessage = document.getElementById("resultMessage");
+    resultMessage.style.display = "none";
 
     var submitButton = document.querySelector('button[type="submit"]');
-    submitButton.disabled = true;
+    submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
     var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
 
+    // Рассчитываем максимальную сумму кредита
+    // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
     var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
 
     setTimeout(function() {
+        submitButton.disabled = false; // Включаем кнопку после загрузки
+
         overlay.style.display = "none";
-        submitButton.disabled = false;
-        document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
-        approvalText.style.display = "block";
-    }, 10000);
+        loadingMessage.style.display = "none";
+        resultMessage.style.display = "block";
+        resultMessage.innerText = "Ваша заявка предварительно одобрена. Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
+    }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
 });
