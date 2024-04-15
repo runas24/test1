11be812b829d7e1485ec712a962ed4cd48763901
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-15 15:45:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+function formatCurrency(input) {
+    var value = input.value.replace(/\D/g, '');
+    var formattedValue = new Intl.NumberFormat('ru-RU').format(value);
+    input.value = formattedValue;
+}
+
 document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var submitButton = document.querySelector('button[type="submit"]');
+    var submitButton = document.getElementById('submitButton');
     submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
-    // Показываем прогресс и текст "Заявка на рассмотрении"
-    document.getElementById("overlay").style.display = "flex";
-    document.querySelector(".progress").style.width = "0%";
-    document.getElementById("statusText").innerText = "Заявка на рассмотрении";
+    var loadingMessage = document.getElementById('loading');
+    loadingMessage.style.display = 'block'; // Показываем сообщение о загрузке
 
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
@@ -19,11 +23,11 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
 
     setTimeout(function() {
         submitButton.disabled = false; // Включаем кнопку после загрузки
+        loadingMessage.style.display = 'none'; // Скрываем сообщение о загрузке
+
+        var approvalMessage = document.getElementById('approvalMessage');
+        approvalMessage.style.display = 'block'; // Показываем сообщение о предварительном одобрении
 
         document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
-
-        // Скрываем прогресс и меняем текст на "Ваша заявка предварительно одобрена"
-        document.getElementById("overlay").style.display = "none";
-        document.getElementById("statusText").innerText = "Ваша заявка предварительно одобрена";
     }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
 });
